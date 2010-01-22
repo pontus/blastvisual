5fd252d7f0f7118c11833ba2428a2d7c90eb9797
Pontus Freyhult <pontus@storskarven.(none)>
2010-01-22 15:46:40 +0100

    New helper functions, some refactoring.

--- blastutil.php
+++ blastutil.php
@@ -67,66 +67,72 @@ function dbs_per_prog($p)
   return $ret;
 }
 
-function transform_coordinate($gene,$offset)
+
+
+function parse_gfffile($gene)
 {
   global $config;
-  static $cdses = 0;
-  static $cdsforgene = '';
-
   $gfffile = $config['cdsgff'];
   
   $f = file($gfffile,FILE_SKIP_EMPTY_LINES);
+  
+  $cdsforgene = $gene;
+  $cdses = array();
+  
+  // For each line in file
+  foreach ($f as $line) 
+    {
+      // Tab separated
+      $l = explode("\t", $line);
+      
+      if ($l[0] == $gene && $l[2] == 'CDS') // This line matters to us?
+	{
+	  $cdses[] = array_slice($l,3,2);  
+	}
+    }
+  
+  function cmp($a, $b)
+  {
+    // Normal handling of first index
+    if ($a[0] < $b[0])
+      {
+	return -1;
+      }
+    
+    if ($a[0] > $b[0])
+      {
+	return 1;
+      }
+    
+    // First index same? (Probably shouldn't happen, but)
+    
+    if ($a[1] == $b[1])
+      {
+	// Same
+	return 0;
+	  }
+    
+    return ($a[1] < $b[1]) ? -1 : 1;
+  }
+  
+  
+  // Sort so we don't need to go through the entire array 
+  usort($cdses, "cmp");            
+}
 
+function transform_coordinate($gene,$offset)
+{
+
+  static $cdses = 0;
+  static $cdsforgene = '';
 
   if ($gene != $cdsforgene)
     {
       // Don't do this again if we don't have to
       $cdsforgene = $gene;
-      $cdses = array();
-      
-      // For each line in file
-      foreach ($f as $line) 
-	{
-	  $l = explode("\t", $line);
-	  
-	  if ($l[0] == $gene && $l[2] == 'CDS') // This line matters to us?
-	    {
-	      $cdses[] = array_slice($l,3,2);  
-	    }
-	}
-      
-      function cmp($a, $b)
-      {
-	// Normal handling of first index
-	if ($a[0] < $b[0])
-	  {
-	    return -1;
-	  }
-	
-	if ($a[0] > $b[0])
-	  {
-	    return 1;
-	  }
-	
-	// First index same? (Probably shouldn't happen, but)
-	
-	if ($a[1] == $b[1])
-	  {
-	    // Same
-	    return 0;
-	  }
-	
-	return ($a[1] < $b[1]) ? -1 : 1;
-      }
-      
-
-      // Sort so we don't need to go through the entire array 
-      usort($cdses, "cmp");
-            
+      $cdses = parse_gfffile($gene);
     }
-  
-
-  
+    
   $i = 0;
   $pos = 0;
   // While what's missing is more than the length of the current
@@ -138,10 +144,31 @@ function transform_coordinate($gene,$offset)
 
   // Offseted position = remainder + CDS tart
   $offsetedcoord = $cdses[$i][0]+($offset-$pos);
-  print "coord : $i, $offsetedcoord\n";
-
 }
 
+function extron_limits($gene, $offseted_coord)
+{
+
+  static $cdses = 0;
+  static $cdsforgene = '';
+
+  if ($gene != $cdsforgene)
+    {
+      // Don't do this again if we don't have to
+      $cdsforgene = $gene;
+      $cdses = parse_gfffile($gene);
+    }
+
+  foreach($cdses as $current_cds)
+    {
+      if (($offseted_coord >= $current_cds[0]) &&
+	  ($offseted_coord =< $current_cds[1]))
+
+	return $current_cds;
+    }
+
+  return false;
+}
 
 
 ?>
